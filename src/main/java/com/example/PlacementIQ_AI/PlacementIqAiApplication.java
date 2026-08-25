ef4862e6b8fefcbf05ac529aab5182c71027b863
ssahuji910@gmail.com <ssahuji910@gmail.com>
2026-08-25 22:41:07 +0530

    first commit

---
+++ src/main/java/com/example/PlacementIQ_AI/PlacementIqAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.PlacementIQ_AI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacementIqAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacementIqAiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/PlacementIQ_AI/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.PlacementIQ_AI.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+    @Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String role;
+}
+    
