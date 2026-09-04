48d904de0176ec928bdd1b7a9800fa415cd893aa
ssahuji910@gmail.com <ssahuji910@gmail.com>
2026-09-04 22:04:07 +0530

    Assesments parts initiate

---
+++ placementiq-frontend/src/pages/AptitudeTest.tsx
@@ -0,0 +1,23 @@
+function AptitudeTest() {
+  return (
+    <div className="container py-5">
+      <h2 className="fw-bold">
+        Aptitude Test - Round 1
+      </h2>
+
+      <p>
+        Total Questions: 20
+      </p>
+
+      <p>
+        Pass Criteria: 60%
+      </p>
+
+      <button className="btn btn-primary">
+        Start Test
+      </button>
+    </div>
+  );
+}
+
+export default AptitudeTest;
